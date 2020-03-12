710e3dc118fb08903826210165e47e0d8e9ca7d2
pushkarpm24 <pushkarmorey555@gmail.com>
2020-03-12 12:32:15 +0530

    ADDED: Code for Part time employee wage

--- employeewage.sh
+++ employeewage.sh
@@ -1,15 +1,19 @@
 #!/bin/bash -x
 
-isPresent=1;
-randomCheck=$((RANDOM%2));
+isPartTime=1
+isFullTime=2
+empRatePerHr=20
+randomCheck=$((RANDOM%3))
 
-if [ $isPresent -eq $randomCheck ];
+if [[ $isFullTime -eq $randomCheck ]]
 then
-	empRatePerHr=20;
-	empHrs=8;
-	salary=$(($empHrs*$empRatePerHr));
-	echo $salary
+	empHrs=8
+elif [[ $isPartTime -eq $randomCheck ]]
+then
+	empHrs=4
 else
-	salary=0;
-	echo $salary
+	empHrs=0
 fi
+
+salary=$(($empHrs*$empRatePerHr))
+
